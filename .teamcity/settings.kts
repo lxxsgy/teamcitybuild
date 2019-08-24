22da134284eaf91c98e9b5da4353d3598396dde4
shiguangyun <guangyun.shi@gatherresource.com>
2019-08-24 22:38:09 +0800

    ee

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -22,7 +22,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2019.1"
+version = "2018.2"
 
 project {
 
