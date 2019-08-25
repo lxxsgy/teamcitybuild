1170fb17ad15bb812933935f1bf08d49b16d8be8
shiguangyun <guangyun.shi@gatherresource.com>
2019-08-25 17:17:08 +0800

    提交setting

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,14 +1,21 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 
 /*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
+The settings script is an entry point for defining a single
+TeamCity project. TeamCity looks for the 'settings.kts' file in a
+project directory and runs it if it's found, so the script name
+shouldn't be changed and its package should be the same as the
+project's id.
 
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
+The script should contain a single call to the project() function
+with a Project instance or an init function as an argument.
+
+VcsRoots, BuildTypes, and Templates of this project must be
+registered inside project using the vcsRoot(), buildType(), and
+template() methods respectively.
+
+Subprojects can be defined either in their own settings.kts or by
+calling the subProjects() method in this project.
 
 To debug settings scripts in command-line, run the
 
@@ -16,54 +23,11 @@ To debug settings scripts in command-line, run the
 
 command and attach your debugger to the port 8000.
 
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View ->
+Tool Windows -> Maven Projects), find the generate task
+node (Plugins -> teamcity-configs -> teamcity-configs:generate),
+the 'Debug' option is available in the context menu for the task.
 */
 
-version = "2019.1"
-
-project {
-    description = "Contains all other projects"
-
-    features {
-        feature {
-            id = "PROJECT_EXT_1"
-            type = "ReportTab"
-            param("startPage", "coverage.zip!index.html")
-            param("title", "Code Coverage")
-            param("type", "BuildReportTab")
-        }
-        feature {
-            id = "PROJECT_EXT_3"
-            type = "OAuthProvider"
-            param("clientId", "lxxsgy")
-            param("defaultTokenScope", "public_repo,repo,repo:status,write:repo_hook")
-            param("secure:clientSecret", "credentialsJSON:52cecdf4-be91-49ec-822d-f14148b1e46f")
-            param("displayName", "GitHub.com")
-            param("gitHubUrl", "https://github.com/")
-            param("providerType", "GitHub")
-        }
-        feature {
-            id = "PROJECT_EXT_5"
-            type = "OAuthProvider"
-            param("secure:userPass", "credentialsJSON:52cecdf4-be91-49ec-822d-f14148b1e46f")
-            param("displayName", "Docker Registry")
-            param("userName", "sgylxx")
-            param("providerType", "Docker")
-            param("repositoryUrl", "https://registry.cn-hongkong.aliyuncs.com")
-        }
-    }
-
-    cleanup {
-        preventDependencyCleanup = false
-    }
-
-    subProject(Test)
-}
-
-
-object Test : Project({
-    name = "test"
-})
+version = "2018.2"
+project(_Self.Project)
