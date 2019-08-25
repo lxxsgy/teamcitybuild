0db2985c3587db7195e16d4b52d1517a4aa2a0c3
shiguangyun <guangyun.shi@gatherresource.com>
2019-08-25 21:40:28 +0800

    修改master

--- .teamcity/GnWorkLoadInquiry/vcsRoots/GnWorkLoadInquiryVcs.kt
+++ .teamcity/GnWorkLoadInquiry/vcsRoots/GnWorkLoadInquiryVcs.kt
@@ -6,7 +6,7 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
 object GnWorkLoadInquiryVcs : GitVcsRoot({
     name = "https://github.com/lxxsgy/GN.WorkLoad.Inquiry/#refs/heads/master"
     url = "https://github.com/lxxsgy/GN.WorkLoad.Inquiry.git"
-    branch = "refs/heads/dev"
+    branch = "refs/heads/master"
     authMethod = password {
         userName = "lxxsgy"
         password = "sgylxx520"
